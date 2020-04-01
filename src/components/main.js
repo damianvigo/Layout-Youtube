598d9c48bf1da4d37adfc039398c22e5b4bce311
Damian <damiandanielvigo@gmail.com>
2020-04-01 06:35:46 -0300

    mediaqueries-rdy

--- src/components/main.js
+++ src/components/main.js
@@ -3,3 +3,17 @@ const contenedor = document.querySelector('#contenedor');
 document.querySelector('#boton-menu').addEventListener('click', () => {
   contenedor.classList.toggle('active');
 });
+
+const comprobarAncho = () => {
+  if(window.innerWidth <= 768) {
+    contenedor.classList.remove('active');
+  } else {
+    contenedor.classList.add('active');
+  }
+}
+
+comprobarAncho();
+
+window.addEventListener('resize', () => {
+    comprobarAncho();
+});
